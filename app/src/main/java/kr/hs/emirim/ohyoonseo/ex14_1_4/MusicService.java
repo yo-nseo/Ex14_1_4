e0b06c897015d15dfe1617dfbaad25bf480b463a
YoonSeo4618 <w2035@e-mirim.hs.kr>
2021-08-24 20:17:47 +0900

    commit

--- app/src/main/java/kr/hs/emirim/ohyoonseo/ex14_1_4/MusicService.java
+++ app/src/main/java/kr/hs/emirim/ohyoonseo/ex14_1_4/MusicService.java
@@ -2,11 +2,13 @@ package kr.hs.emirim.ohyoonseo.ex14_1_4;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MusicService extends Service {
     public MusicService() {
-        
+
     }
 
     @Override
@@ -14,4 +16,23 @@ public class MusicService extends Service {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
+    @Override
+    public void onCreate() {
+        Log.i("서비스 테스트::", "onCreate() 호출됨");
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("서비스 테스트::", "onStartCommand() 호출됨");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i("서비스 테스트::", "onDestroy() 호출됨");
+        super.onDestroy();
+    }
+
 }
